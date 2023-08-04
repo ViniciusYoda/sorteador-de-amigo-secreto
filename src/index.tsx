05aa30e246007a9a6fdc614baf99595b4770e73c
Vinícius Yoda <vyoda4604@gmail.com>
2023-08-04 07:31:36 -0300

    test

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-
+import Cabecalho from './componentes/Cabecalho';
 
 ReactDOM.render(
   <React.StrictMode>
+    <Cabecalho />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
-
--- src/state/atom.ts
+++ src/state/atom.ts
@@ -1,8 +1,13 @@
 import { atom } from "recoil";
 
-export const listaparticipantesState = atom<string[]>({
+export const listaParticipantesState = atom<string[]>({
     key: 'listaParticipantesState',
     default: []
+})
+
+export const resultadoAmigoSecreto = atom<Map<string, string>>({
+    key: 'resultadoAmigoSecreto',
+    default: new Map()
 })
 
 export const erroState = atom<string>({
